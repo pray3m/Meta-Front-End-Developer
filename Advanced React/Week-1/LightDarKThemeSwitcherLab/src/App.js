1c807cc17a29e1ef4cb0df42a3893d2d8a4b9107
pray3m <prem.gtm9@gmail.com>
2023-09-28 20:43:33 +0545

    React Context: Light dark theme switcher lab

--- Advanced React/Week-1/LightDarKThemeSwitcherLab/src/App.js
+++ Advanced React/Week-1/LightDarKThemeSwitcherLab/src/App.js
@@ -34,8 +34,8 @@ const Content = () => {
       <Paragraph>
         We are a pizza loving family. And for years, I searched and searched and
         searched for the perfect pizza dough recipe. I tried dozens, or more.
-        And while some were good, none of them were that recipe that would
-        make me stop trying all of the others.
+        And while some were good, none of them were that recipe that would make
+        me stop trying all of the others.
       </Paragraph>
     </div>
   );
--- Advanced React/Week-1/LightDarKThemeSwitcherLab/src/ThemeContext.js
+++ Advanced React/Week-1/LightDarKThemeSwitcherLab/src/ThemeContext.js
@@ -1,22 +1,20 @@
-import {createContext,useState, useContext} from "react";
+import { createContext, useState, useContext } from "react";
 
-const ThemeContext= createContext(undefined);
+const ThemeContext = createContext(undefined);
 
 export const ThemeProvider = ({ children }) => {
-    const [theme,setTheme] = useState("light");
+  const [theme, setTheme] = useState("light");
 
-    return (
-        <ThemeContext.Provider
-        value={{
-            theme,
-            toggleTheme : ()=> setTheme(theme ==="light" ? "dark" : "light")
-        }}
-        >
-        {children}
-        </ThemeContext.Provider>
-    )
-}
-
-
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme: () => setTheme(theme === "light" ? "dark" : "light"),
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
 
 export const useTheme = () => useContext(ThemeContext);
